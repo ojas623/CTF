c81b7fe20cc8e16fb2110ce8f1eb34a6124e9359
Ojas Sathe <83601895+ojas623@users.noreply.github.com>
2022-02-26 20:09:59 -0800

    picoCTF 2019 forensics - Shark on wire 1

---
+++ forensics/sharkOnWire1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#make sure capture.pcap is in same directory as this script
+
+from scapy.all import *
+
+pcap = rdpcap("capture.pcap")
+
+flag = []
+
+for p in pcap[UDP]:
+    try:
+        if(p[IP].src == "10.0.0.2" and p[IP].dst == "10.0.0.12"):
+            flag.append(p[Raw].load.decode('utf-8'))
+    except IndexError:
+        continue
+
+print("".join(flag))
